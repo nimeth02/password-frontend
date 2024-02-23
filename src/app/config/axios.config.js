ab2a1ccbc0e9506ed1b77ee09a0ce1e634513180
Nimeth <90857630+nimeth02@users.noreply.github.com>
2024-02-23 11:24:57 +0530

    Update axios.config.js

--- src/app/config/axios.config.js
+++ src/app/config/axios.config.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
 
-console.log(JSON.parse(localStorage.getItem("userInfo")))
+ if (typeof window !== 'undefined') {
+                       console.log(JSON.parse(localStorage.getItem("userInfo")))
+                    }
+
 
 export const axiosInstance = axios.create({
   baseURL: 'http://localhost:3001',
   headers: {
-    Authorization: `Bearer ${JSON.parse(localStorage.getItem("userInfo"))}`
+    Authorization: `Bearer ${ typeof window !== 'undefined' ? JSON.parse(localStorage.getItem("userInfo")) : ''}`
   }
 });
