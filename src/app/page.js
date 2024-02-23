b800b30a3711a1f53fa7a675641289d8e395dd87
Nimeth <90857630+nimeth02@users.noreply.github.com>
2024-02-23 11:19:55 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -9,9 +9,12 @@ import CardOne from "./card";
 function Home() {
   const router = useRouter()
   useEffect(()=>{
-    const user = JSON.parse(localStorage.getItem("userInfo"));
+     if (typeof window !== 'undefined') {
+                      const user = JSON.parse(localStorage.getItem("userInfo"));
     if (!user) {router.push("/signin")}
     else{router.push("/")}
+                    }
+   
   },[])
   return (
     <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-20 md:p-40 w-full gap-10'>
